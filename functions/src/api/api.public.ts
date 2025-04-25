83f2a7c1d510825b43238ccfe8668a40183b9ea4
Tien Nguyen <nht@nguyenhoatien.com>
2025-04-25 09:49:44 +0700

    feat: add CORS support to API and update dependencies

--- functions/src/api/api.public.ts
+++ functions/src/api/api.public.ts
@@ -1,8 +1,11 @@
 import {onRequest} from 'firebase-functions/v2/https';
 import express from 'express';
+import cors from 'cors';
 import verifyRoutes from './public/verify';
 
 const app = express();
+
+app.use(cors({origin: true}));
 
 app.get('/api/public/', async (req, res) => {
   res.send('Hello world!');
